04ab959b2f7dd4df9dab0035d9a36919b8a20549
Stefanos <165141357+Stefanos0710@users.noreply.github.com>
2026-05-26 14:48:03 +0200

    Add camera setup and AprilTag detection functionality

--- firmware/drone/camera.py
+++ firmware/drone/camera.py
@@ -19,5 +19,49 @@ Waht I want to happen get video stream from teh camera,
 then get the april tag and its position
 """
 
-import cv2
-import pupil_apriltags
+import argparse
+import cv2 as cv
+from pupil_apriltags import Detector
+
+# defining the arguments of the parser
+ap = argparse.ArgumentParser()
+ap.add_argument("--width", type=int, default=640, help="the width of the video stream")
+ap.add_argument("--height", type=int, default=480, help="the height of the video stream")
+ap.add_argument("--families", type=str, default="tagCustom48h12", help="the family of the april tag to detect")
+ap.add_argument("--nthreads", type=int, default=3, help="the number of threads to use for the detection (cpu cores)") # resberry pi zero 2 W has 4 cores, so using 3 of them for detection
+ap.add_argument("--quad_decimate", type=float, default=1.5, help="decimate input image by this factor")
+
+# pars the arguments
+args = ap.parse_args()
+
+# setup the camera
+cap = cv.VideoCapture(1)
+cap.set(cv.CAP_PROP_FRAME_WIDTH, args.width)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, args.height)
+
+# setup the april tag detector
+at_detector = Detector(
+    families=args.families, 
+    nthreads=args.nthreads, 
+    quad_decimate=args.quad_decimate
+)
+
+try:
+    while True:
+        # read a frame from the camera
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Failed to grab frame")
+            break
+
+        image = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        tags = at_detector.detect(image)
+
+        # print the detected tags and their positions
+        for tag in tags:
+            print(f"Detected tag with id {tag.tag_id} at position {tag.center}")
+
+except KeyboardInterrupt:
+    print("breaking the loop... T-T")
+    cap.release()
