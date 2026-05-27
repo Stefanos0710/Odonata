1a4dcab391e9bec4c1f58687b717c9f038416700
Stefanos <165141357+Stefanos0710@users.noreply.github.com>
2026-05-27 22:23:19 +0200

    Add drone connection and command classes to main.py for improved API functionality

--- firmware/server/main.py
+++ firmware/server/main.py
@@ -1,9 +1,11 @@
 from os import path
-
+import requests
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from pymavlink import mavutil
 
 app = FastAPI()
 
@@ -12,6 +14,38 @@ if path.exists("frontend"):
 
 templates = Jinja2Templates(directory="frontend")
 
+pi_ip = "11.22.33.44" # replace with the actual ip address of the pi
+pi_url = f"http://{pi_ip}:5000/api"
+
+master = None
+
+def get_laptop_drone_connection():
+    global master
+    if master is None:
+        master = mavutil.mavlink_connection("udpin:0.0.0.0:14550")
+    return master
+
+class ServoCommand(BaseModel):
+    angle: int
+
+class MotorTestCommand(BaseModel):
+    motor_number: int
+    throttle: float
+    duration: float
+
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse(request, "index.html")
+
+@app.post("/api/get_data")
+def get_data():
+    # set battery status
+    battery_percentage = 0
+    battery_connection = False
+
+    drone = get_laptop_drone_connection()
+    msg = drone.recv_match(type='SYS_STATUS', blocking=False)
+    if msg is not None:
+        battery_percentage = msg.battery_remaining
+        battery_connection = True
+
